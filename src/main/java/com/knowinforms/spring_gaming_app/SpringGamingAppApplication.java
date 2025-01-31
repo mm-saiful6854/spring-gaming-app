16f38b77e8a3fd4819177194be3fd91a849360b1
mm-saiful6854 <cse.saiful2119@gmail.com>
2025-01-31 20:15:04 +0600

    init

---
+++ src/main/java/com/knowinforms/spring_gaming_app/SpringGamingAppApplication.java
@@ -0,0 +1,12 @@
+package com.knowinforms.spring_gaming_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGamingAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGamingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knowinforms/spring_gaming_app/SpringGamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowinforms.spring_gaming_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGamingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
