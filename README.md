8654cab5433d5606f1f01676b39b67d3696f24f9
mm-saiful6854 <cse.saiful2119@gmail.com>
2025-02-07 21:19:55 +0600

    add spring actuator update readme file

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 Spring Gaming Application
 ===============================
-Spring-gaming-app is a project to explain what is tightly couple, loosing couple, and how spring can make a class loosing coupled. What are the benefits a class gets after being loosing coupled. 
+Spring-gaming-app is a project to explain what is tightly couple, loosing couple, and how spring can make 
+a class loosing coupled. What are the benefits a class gets after being loosing coupled. 
 ### What is tightly couple class?
+Tightly coupled classes are firmly interdependent to each other. If one class is facing any change request
+another is expected to be changed. For example, if an Authenticator class depends on User class, 
+then any changes on User class will be affected on Authenticator class.
 ### What is loosing couple class?
 ### How does spring make class loosing couple?
 
--- src/main/java/com/knowinforms/spring_gaming_app/SpringGamingAppApplication.java
+++ src/main/java/com/knowinforms/spring_gaming_app/SpringGamingAppApplication.java
@@ -1,9 +1,6 @@
 package com.knowinforms.spring_gaming_app;
 
-import com.knowinforms.spring_gaming_app.game.Game;
 import com.knowinforms.spring_gaming_app.game.GameRunner;
-import com.knowinforms.spring_gaming_app.game.MarioGame;
-import com.knowinforms.spring_gaming_app.game.SuperContraGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,10 +9,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class SpringGamingAppApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringGamingAppApplication.class, args);
-
-//		Game game = new MarioGame();
-//		Game game1 = new SuperContraGame();
-
 		GameRunner runner = context.getBean(GameRunner.class);
 		runner.run();
 	}
